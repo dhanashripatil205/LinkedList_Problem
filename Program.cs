f33beb9673cbd14cbdca986f28d6ff4ead895259
dhanashripatil205 <dhanashripatil205@gmail.com>
2023-01-11 18:23:13 +0530

    Method Calling

--- Program.cs
+++ Program.cs
@@ -5,9 +5,9 @@
         public static void Main(string[] args)
         {
             LinkedList linkedList = new LinkedList();
-            linkedList.Add(70);
-            linkedList.Add(30);
             linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
             linkedList.Display();
         }
     }
